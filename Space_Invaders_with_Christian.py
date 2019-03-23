812ff213258d3fa74a20ddb8bd00074103baeb96
Stephen Citarella <steve.citarella@gmail.com>
2019-03-23 17:29:55 -0400

    first commit

---
+++ Space_Invaders_with_Christian.py
@@ -0,0 +1,21 @@
+# Space Invaders - Part 1
+# Set up the screen
+# He's typing this on Python 2.7
+
+import turtle, os
+
+#Set up screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
+
+#Draw border
+border_pen = turtle.Turtle()
+border_pen_speed(0) #oddly, "0" is the fastest
+
+
+
+
+
+
+delay = raw_input("Press enter to play")
